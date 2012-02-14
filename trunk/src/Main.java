20d79af0c2020f9e6dee930193ef89c4e884de98
fericss@gmail.com <fericss@gmail.com@9fb70296-360b-1dba-0e86-8c62e50031bf>
2012-02-14 16:47:06 +0000

    Uploaded files

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -1,4 +1,306 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main {
+import javax.swing.JFrame;
+
+
+public class Main extends JFrame{
+	private String user;
+	private String password;
+	String cookie;
+	HttpURLConnection connection = null;
+	URL serverAddress = null;
+	OutputStreamWriter wr = null;
+	BufferedReader rd  = null;
+	StringBuilder sb = null;
+	String rack;
+	//    String line = null;
+
+	String[][] game;
+
+
+	public Main() throws Exception{
+		game = new String[15][15];
+		cookie = getCookie();
+		List<String> gamesIDList = getGames();
+		String _gameInfo = getGame(gamesIDList.get(0));
+		parseTiles(_gameInfo);
+		printTiles();
+		WordFinder find = new WordFinder();
+
+
+
+		List<String> bla;
+		/// TEST
+//		        List<String> build = new ArrayList<String>();
+//		        for(String[] s : game){
+//		            String _tmp ="";
+//		            for(String st : s){
+//		                _tmp += st==null? "" : st;
+//		            }
+//		            System.out.println(_tmp);
+//		            bla = find.Matches(rack+_tmp);
+//		            for(String st : bla){
+////		                System.out.print(st+", ");
+//		                if(!st.equals("") && st.contains(_tmp)){
+//		                    build.add(st);
+//		                }
+//		            }
+//		        }
+//		        for(String s : build){
+//		            System.out.println(s);
+//		        }
+
+		//        /// END TEST
+		System.out.print("Words built with rack: ");
+		bla = find.Matches(rack);
+		for(String s : bla){
+			System.out.print(s+", ");
+		}
+
+	}
+	private void printTiles() {
+		for(String[] s : game){
+			for(String i : s){
+				System.out.print(""+(i==null ? "\t" : "\t"+i));
+			}
+			System.out.println();
+		}
+		System.out.print("Rack: ");
+		System.out.println(rack);
+	}
+	private void parseTiles(String gameInfo) {
+		int pos = 0;
+		while(gameInfo.charAt(pos)!='['){
+			pos++;
+		}
+		boolean done = false;
+		while(!done){
+
+
+			while(gameInfo.charAt(pos)!=','){
+				pos++;
+			}
+			int l1;
+			if(gameInfo.charAt(pos-2)>'0' && gameInfo.charAt(pos-2)<'9'){
+				l1 = Integer.parseInt((""+gameInfo.charAt(pos-2)+""+gameInfo.charAt(pos-1)));
+			}else{
+				l1 = gameInfo.charAt(pos-1)-48;
+			}
+			pos++;
+			while(gameInfo.charAt(pos)!=','){
+				pos++;
+			}
+			int l2;
+			if(gameInfo.charAt(pos-2)>'0' && gameInfo.charAt(pos-2)<'9'){
+				l2 = Integer.parseInt((""+gameInfo.charAt(pos-2)+""+gameInfo.charAt(pos-1)));
+			}else{
+				l2 = gameInfo.charAt(pos-1)-48;
+			}
+			pos++;
+			while(gameInfo.charAt(pos)!=','){
+				pos++;
+			}
+			String l3 = String.valueOf(gameInfo.charAt(pos-2));
+//			System.out.println(""+l1+" "+l2+" "+l3);
+			game[l2][l1] = ""+l3;
+			if(gameInfo.charAt(pos+7)==']'&& gameInfo.charAt(pos+8)==']'){
+				done = true;
+				//                break;
+			}
+			pos = pos + 11;
+		}
+		//        System.out.println("bla");
+		//                while(gameInfo.charAt(pos)!='"' && gameInfo.charAt(pos-1)!='k'
+		//                        && gameInfo.charAt(pos-2)!='c'
+		//                            && gameInfo.charAt(pos-3)!='a'
+		//                                && gameInfo.charAt(pos-4)!='r'
+		//                                    && gameInfo.charAt(pos-5)!='"'){
+		//                    pos++;
+		//                    System.out.println(pos);
+		//                }
+		//                pos+=10;
+		done = false;
+		rack = "";
+
+		while(!done){
+			pos++;
+			//            System.out.println(pos);
+			if(gameInfo.charAt(pos)=='"' && gameInfo.charAt(pos-1)=='k'
+				&& gameInfo.charAt(pos-2)=='c'
+					&& gameInfo.charAt(pos-3)=='a'
+						&& gameInfo.charAt(pos-4)=='r'
+							&& gameInfo.charAt(pos-5)=='"'){
+				done = true;
+				break;
+			}
+		}
+		done = false;
+		while(!done){
+			pos++;
+			if(gameInfo.charAt(pos)>='A' && gameInfo.charAt(pos)<='Z' && gameInfo.charAt(pos-1)=='"' && gameInfo.charAt(pos+1)=='"'){
+				//                System.out.println(""+gameInfo.charAt(pos));
+				rack = rack+gameInfo.charAt(pos);
+			}
+			if(gameInfo.charAt(pos)==']'&& gameInfo.charAt(pos+1)=='}'){
+				done = true;
+				break;
+			}
+		}
+
+
+
+	}
+	private List<String> getGames() throws Exception {
+		ArrayList<String> gameID = new ArrayList<String>();
+		serverAddress = new URL("http://game03.wordfeud.com/wf/user/games/");
+		//        PrintStream utdata = new PrintStream(connection.getOutputStream());
+		connection = (HttpURLConnection)serverAddress.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setDoOutput(true);
+		connection.setReadTimeout(10000);
+		connection.addRequestProperty("Cookie", cookie);
+		connection.connect();
+		rd  = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String line= rd.readLine();
+		//        while ((line = rd.readLine()) != null)
+		//        {
+		System.out.println(line);
+		//        }
+		int pos = 0;
+		while(!line.substring(pos,pos+4).equals("\"id\"")){
+			pos++;
+		}
+		pos = pos+6;
+		//        while(line.charAt(pos)<'0' &&  line.charAt(pos)>'9'){
+		//            pos++;
+		//        }
+		int startPos = pos;
+		while(line.charAt(pos)!=','){
+			pos++;
+		}
+		//        System.out.println(line.substring(startPos,pos));
+		gameID.add(line.substring(startPos,pos));
+
+		return gameID;
+
+		//        System.out.println(str);
+		//        utdata.println(str);
+
+	}
+	private String getGame(String s) throws Exception {
+		serverAddress = new URL("http://game06.wordfeud.com/wf/game/"+s+"/");
+		//        PrintStream utdata = new PrintStream(connection.getOutputStream());
+		connection = (HttpURLConnection)serverAddress.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setDoOutput(true);
+		connection.setReadTimeout(10000);
+		connection.addRequestProperty("Cookie", cookie);
+		connection.connect();
+		rd  = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String line = rd.readLine();
+		//        while ((line = rd.readLine()) != null)
+		//        {
+		System.out.println(line);
+		//        }
+		return line;
+
+		//        System.out.println(str);
+		//        utdata.println(str);
+
+	}
+	public static void main(String args[]) throws Exception{
+		new Main();
+	}
+	public String getCookie() throws Exception{
+		user = "fredric.ericsson@gmail.com";
+		password = "icpmwq";
+		String salt = "JarJarBinks9";
+		//        new Main();
+		serverAddress = new URL("http://game03.wordfeud.com/wf/user/login/email/");
+		//set up out communications stuff
+		connection = null;
+
+		//Set up the initial connection
+		connection = (HttpURLConnection)serverAddress.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setDoOutput(true);
+		connection.setReadTimeout(10000);
+		connection.addRequestProperty("Content-type", "application/json");
+		connection.addRequestProperty("Host","game03.wordfeud.com");
+		connection.addRequestProperty("Connection","Keep-Alive");
+		connection.addRequestProperty("User-Agent","WebFeudClient/1.2.8 (Android 2.2.3)");
+
+		PrintStream utdata = new PrintStream(connection.getOutputStream());
+
+
+		String pwd = SHA1(password+salt);
+
+		String str = "{\"password\": \""+pwd+"\""+","+
+		"\"email\": \""+user+"\"}";
+		System.out.println(str);
+		utdata.println(str);
+		//        System.out.println(connection);
+		connection.connect();
+
+		//        for(String bla : connection.getHeaderFields().keySet()){
+		////            for(String s : bla){
+		////                System.out.println(bla);
+		////            }
+		//           
+		//        }
+		//        connection.
+		String cookie = connection.getHeaderField("Set-Cookie");
+		//        System.out.println(connection.getHeaderField("Set-Cookie"));
+		//        rd  = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		//        while ((line = rd.readLine()) != null)
+		//        {
+		//            System.out.println(line);
+		//        }
+		return cookie;
+	}
+
+	private static String convToHex(byte[] data) {
+		StringBuilder buf = new StringBuilder();
+		for (int i = 0; i < data.length; i++) {
+			int halfbyte = (data[i] >>> 4) & 0x0F;
+			int two_halfs = 0;
+			do {
+				if ((0 <= halfbyte) && (halfbyte <= 9))
+					buf.append((char) ('0' + halfbyte));
+				else
+					buf.append((char) ('a' + (halfbyte - 10)));
+				halfbyte = data[i] & 0x0F;
+			} while(two_halfs++ < 1);
+		}
+		return buf.toString();
+	}
+
+	public static String SHA1(String text) throws NoSuchAlgorithmException,
+	UnsupportedEncodingException  {
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		byte[] sha1hash = new byte[40];
+		md.update(text.getBytes("iso-8859-1"), 0, text.length());
+		sha1hash = md.digest();
+		return convToHex(sha1hash);
+	}
 
 }
---
+++ trunk/src/WordFinder.java
@@ -0,0 +1,158 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class WordFinder {
+	private String[] wordList;
+	private String[][] m_words;
+	public WordFinder(){
+		wordList = LoadWords();
+	}
+	public List<String> Matches(String sourceLetters)
+	{
+		sourceLetters = sourceLetters.toUpperCase();
+		List<String> matches = new ArrayList<String>();
+		for(String word : wordList)
+		{
+			if(word!=null){
+				if (WordCanBeBuiltFromSourceLetters(word.toUpperCase(), sourceLetters))
+					matches.add(word);
+			}
+		}
+		return matches;
+	}
+
+
+	private String[] LoadWords() {
+		m_words = new String[16][];
+		FileInputStream fstream;
+		String[] words = new String[50000];
+		int counter = 0;
+		try {
+			fstream = new FileInputStream("words.txt");
+
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			
+			
+			int numWords = 0;
+			String line=null;
+			while ((line =br.readLine()) != null){
+					numWords++;
+			}
+			words = new String[numWords];
+//			System.out.println(""+numWords);
+			br.close();
+			in.close();
+			fstream.close();
+			
+			fstream = new FileInputStream("words.txt");
+			in = new DataInputStream(fstream);
+			br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			while ((strLine = br.readLine()) != null)   {
+				words[counter] = strLine;
+				counter++;
+			}
+			br.close();
+			in.close();
+			fstream.close();
+			
+			/// Create some arrays with words of certain lengths, for example m_words[5] has all words of length 5
+			for (int i = 2; i <= 15; i++) {
+				fstream = new FileInputStream("words.txt");
+				in = new DataInputStream(fstream);
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+				// Count the number of lines in the file....
+				numWords = 0;
+				line=null;
+				while ((line =bufferedReader.readLine()) != null){
+					if(line.length()==i)
+						numWords++;
+				}
+
+				br.close();
+				in.close();
+				fstream.close();
+				m_words[i] = new String[numWords];
+				fstream = new FileInputStream("words.txt");
+				in = new DataInputStream(fstream);
+				bufferedReader = new BufferedReader(new InputStreamReader(in));
+				numWords = 0;
+				line=null;
+				while ((line =bufferedReader.readLine()) != null){
+					if(line.length()==i){
+						m_words[i][numWords] = line;
+						numWords++;
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return words;
+	}
+	public boolean WordCanBeBuiltFromSourceLetters(String targetWord, String sourceLetters)
+	{
+		String builtWord = "";
+		char[] letters = targetWord.toCharArray();
+		for(char letter : letters)
+		{
+			
+//			char letter = targetWord.charAt(i);
+			int pos = sourceLetters.indexOf(letter);
+//			System.out.println(""+letter+" "+pos);
+			if (pos >= 0)
+			{
+//				System.out.println("bla");
+				builtWord += letter;
+				sourceLetters = Remove(pos, sourceLetters);
+				continue;
+			}
+
+
+			// check for wildcard
+			pos = sourceLetters.indexOf(".");
+			if (pos >= 0)
+			{
+				builtWord += letter;
+
+				sourceLetters = Remove(pos, sourceLetters);
+			}
+		}
+		return builtWord.equals(targetWord);
+	}
+	private String Remove(int pos, String sourceLetters) {
+		// TODO Auto-generated method stub
+		char[] wat = sourceLetters.toCharArray();
+		for(int i = pos+1;i<wat.length;i++){
+			wat[i-1] = wat[i];
+			wat[i] = 0;
+		}
+		return (""+String.valueOf(wat));
+	}
+	public static void main(String[] args){
+		WordFinder wordList = new WordFinder();
+		System.out.println(wordList.isWord("crayon"));
+		System.out.println(wordList.isWord("cat"));
+		System.out.println(wordList.isWord("qrztru"));
+	}
+	public boolean isWord(String s) {
+		if (s.length() < 2 || s.length() > 15)
+			return false;
+		String[] w = m_words[s.length()];
+		return Arrays.binarySearch(w, s) >= 0;
+	}
+}
