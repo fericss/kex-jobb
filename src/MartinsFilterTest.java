a59cb6fd7491a9054656d0275bd05ebe8837df80
mbernt.mbernt@gmail.com <mbernt.mbernt@gmail.com@9fb70296-360b-1dba-0e86-8c62e50031bf>
2012-03-09 18:18:42 +0000

    TEST

--- src/MartinsFilterTest.java
+++ src/MartinsFilterTest.java
@@ -59,10 +59,25 @@ public class MartinsFilterTest {
 //		timingTest(rack,wordsOnRow,wordlist,1000);
 	}
 	
+	public ArrayList<ArrayList<String>[]> testSlowFilter2(GameInfo gi,WordFinder find, final int rowIndex,boolean vertical){
+		ArrayList<ArrayList<String>[]> list=new ArrayList<ArrayList<String>[]>();
+		//get variables, first three, 3 ms
+		final String rack=gi.getRack();
+		final String row=gi.getRow(rowIndex, false);
+		final String[][] fastCrossers=Help.fastCrossers(gi.getRowCrossers(rowIndex, vertical));
+		final boolean[] impossible =Help.impossible(fastCrossers, find); //7 ms
+		//get for all lengths
+//		for(int length=2;length<=15;length++){
+//			list.add(this.slowFilter2(rack, row, length, fastCrossers, impossible, find));
+//		}
+		list=slowFilter3(rack, row,fastCrossers, impossible, find);
+		return list;
+	}
+	
 	public static int maxDoubles(final String[] wordlist){
 		int max=0;
 		for(int i=0;i<wordlist.length;i++){
-			int tmp=max(FastFilter.createFreq(wordlist[i]));
+			int tmp=max(Help.createFreq(wordlist[i]));
 			if(tmp>max){
 				max=tmp;
 			}
@@ -230,7 +245,29 @@ public class MartinsFilterTest {
 		}
 	}
 	private static int[] getHasChars(final String s,final int wildCards,final int unknown){
-		return getHasChars(FastFilter.createFreq(s), wildCards);
+		return getHasChars(Help.createFreq(s), wildCards);
+	}
+	
+	
+	private static int[] getHasChars(final byte[] charFreq){
+		int max=0;
+		for(int i=0;i<charFreq.length;i++){
+			if(charFreq[i]>max){
+				max=charFreq[i];
+			}
+		}
+		int[] res=new int[max];
+		int tmp;
+		for(int j=0;j<max;j++){
+			tmp=0;
+			for(int i=0;i<charFreq.length;i++){
+				if(charFreq[i]>j){
+					tmp=tmp | (1<<i);
+				}
+			}
+			res[j]=tmp;
+		}
+		return res;
 	}
 	
 	private static int[] getHasChars(final byte[] charFreq, final int wildCards){
@@ -275,9 +312,9 @@ public class MartinsFilterTest {
 		rack=rack.replaceAll("\\.", "");
 		wildcards=wildcards-rack.length();
 		//create source letters string
-		String sourceLetters=rack+concatinate(wordsOnRow);
+		String sourceLetters=rack+Help.concatinate(wordsOnRow);
 		//create freq array from the source
-		final byte[] hasFreq=FastFilter.createFreq(sourceLetters);
+		final byte[] hasFreq=Help.createFreq(sourceLetters);
 		//create hasChars from freq and wildcards
 		final int[] hasChars=getHasChars(hasFreq,wildcards);
 		
@@ -320,7 +357,7 @@ public class MartinsFilterTest {
 					//check for matches if there enough sourceLetters
 					if(boardLetters.length()+racklength>=length){
 						//create freq array from the source
-						final byte[] hasFreq=FastFilter.createFreq(sourceLetters);
+						final byte[] hasFreq=Help.createFreq(sourceLetters);
 						//create hasChars from freq and wildcards
 						final int[] hasChars=getHasChars(hasFreq,wildcards);
 //							res.add("empty");
@@ -370,6 +407,283 @@ public class MartinsFilterTest {
 //		}
 		return res;
 	}
+	
+	/**
+	 * all input must be lowercase.
+	 * @param rack
+	 * @param row
+	 * @param length
+	 * @param fastCrossers
+	 * @param impossible
+	 * @param wf
+	 * @return
+	 */
+	public ArrayList<String>[] slowFilter2(String rack, String row,final int length,
+			final String[][] fastCrossers,final boolean[] impossible,final WordFinder wf){
+		int racklength=rack.length();
+		rack=rack.toLowerCase();
+		row=row.toLowerCase();
+		int wildcards=rack.length();
+		rack=rack.replaceAll("\\.", "");
+		wildcards=wildcards-rack.length();
+		
+		boolean[][] possible=Help.possible(fastCrossers, wf);
+		
+		HashMap<String, ArrayList<String>> map=new HashMap<String, ArrayList<String>>();
+		
+		final String [] sarr=MartinsFilterTest.getCharCombinations4(racklength,row,length,fastCrossers,impossible);
+//		System.out.println(Arrays.toString(sarr));
+//		System.out.println(length+Arrays.toString(sarr));
+		@SuppressWarnings("unchecked")
+		final ArrayList<String>[] res =new ArrayList[sarr.length];
+		for(int j=0;j<res.length;j++){
+			//initialize ArrayList
+			res[j]=new ArrayList<String>();
+			if(sarr[j]!=null){
+//				if(map.containsKey(sarr[j])){
+//					//reuse previous calculation
+//					res[j]=map.get(sarr[j]);
+//				} else {
+					String boardLetters=sarr[j].replaceAll(".","");
+					//create source letters string
+					final String sourceLetters=rack+boardLetters;
+					//check for matches if there enough sourceLetters
+					if(boardLetters.length()+racklength>=length){
+						//create freq array from the source
+						final byte[] hasFreq=Help.createFreq(sourceLetters);
+						//create hasChars from freq and wildcards
+						final int[] hasChars=getHasChars(hasFreq);
+//							res.add("empty");
+						//if there is no letters on board there is no pattern
+						boolean skipPattern=boardLetters.length()==0;
+						Pattern p=Pattern.compile(sarr[j]);
+						
+						//do heavy filtering, everything else 60 ms, everything else in this method 50 ms
+						for(int i=0;i<wordlist.length;i++){
+							if(wordlist[i].length()==length 
+									&& hasNeededChars(i,hasChars,wildcards) && (skipPattern || p.matcher(wordlist[i]).matches())
+									//(p.matcher(wordlist[i]).matches())
+									//&& Help.correctCrossing(wordlist[i],j,fastCrossers,wf)
+									&& Help.correctCrossing(wordlist[i], j, possible)
+									){
+								res[j].add(wordlist[i]);
+							}
+						}
+					}
+					//save the calculation so that it can be reused if the same pattern is used
+//					map.put(sarr[j], res[j]);
+//				}
+			}
+		}
+		return res;
+	} 
+	
+	/**
+	 * twice as fast as slowfilter2
+	 * @param rack
+	 * @param row
+	 * @param length
+	 * @param fastCrossers
+	 * @param impossible
+	 * @param wf
+	 * @return
+	 */
+	public ArrayList<ArrayList<String>[]> slowFilter3(String rack, String row,
+			final String[][] fastCrossers,final boolean[] impossible,final WordFinder wf){
+		
+		
+		int racklength=rack.length();
+		rack=rack.toLowerCase();
+		row=row.toLowerCase();
+		int wildcards=rack.length();
+		rack=rack.replaceAll("\\.", "");
+		wildcards=wildcards-rack.length();
+		
+		final boolean[][] possible=Help.possible(fastCrossers, wf);
+		
+		int size=15-1;
+		//data about row and position
+		final String[][] comb=new String[size][];
+		final int[][][] hasCharss=new int[size][][];
+//		final boolean[][] skipPatterns=new boolean[size][];
+		final Pattern[][] patterns=new Pattern[size][];
+		
+		//Initialize
+		HashMap<String, ArrayList<String>> map=new HashMap<String, ArrayList<String>>();
+		ArrayList<ArrayList<String>[]> res=new ArrayList<ArrayList<String>[]>();
+		
+		//
+		for(int length=2,index=0;length<=15;length++,index++){
+			////Real, get the combinations for a length
+			comb[index]=MartinsFilterTest.getCharCombinations4(racklength,row,length,fastCrossers,impossible);
+			//Initialize
+//			skipPatterns[index]=new boolean[comb[index].length];
+			hasCharss[index]=new int[comb[index].length][];
+			patterns[index]=new Pattern[comb[index].length];
+			//get properties for each comb
+			for(int i=0;i<comb[index].length;i++){
+				if(comb[index][i]!=null){
+					//Initialize
+					final String boardLetters=comb[index][i].replaceAll(".","");
+					final String sourceLetters=rack+boardLetters;
+					final int[] hasChars=getHasChars(Help.createFreq(sourceLetters));
+					//Real
+//					skipPatterns[index][i]=boardLetters.length()==0;
+					//Real
+					hasCharss[index][i]=hasChars;
+					//Real
+					patterns[index][i]=boardLetters.length()==0?null:Pattern.compile(comb[index][i]);
+				}
+			}
+			//Initialize
+			@SuppressWarnings("unchecked")
+			ArrayList<String>[] tmp =new ArrayList[comb[index].length];
+			for(int i=0;i<tmp.length;i++){
+				tmp[i]=new ArrayList<String>();
+			}
+			res.add(tmp);
+		}
+		
+		//do filtering
+		//for each word
+		for(int i=0;i<wordlist.length;i++){
+			final String word=wordlist[i];
+			final int index=word.length()-2;
+//			final ArrayList<String>[] res1=res.get(index);
+			final String sarr[]=comb[index];
+			//for each starting position on the row for the word
+			for(int j=0;j<sarr.length;j++){
+				if(sarr[j]!=null){
+					//retrieve data about position
+					final int[] hasChars=hasCharss[index][j];
+					final Pattern p=patterns[index][j];
+					//check, ha enough chars in rack
+					if(hasNeededChars(i,hasChars,wildcards) && (p==null || p.matcher(word).matches())
+							//(p.matcher(wordlist[i]).matches())
+							//&& Help.correctCrossing(wordlist[i],j,fastCrossers,wf)
+							&& Help.correctCrossing(word, j, possible)
+							){
+//						res1[j].add(word);
+						res.get(index)[j].add(word);
+					}
+						
+				}
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+////		System.out.println(Arrays.toString(sarr));
+////		System.out.println(length+Arrays.toString(sarr));
+//		@SuppressWarnings("unchecked")
+//		final ArrayList<String>[] res =new ArrayList[sarr.length];
+//		
+//		for(int j=0;j<res.length;j++){
+//			//initialize ArrayList
+//			res[j]=new ArrayList<String>();
+//			if(sarr[j]!=null){
+////				if(map.containsKey(sarr[j])){
+////					//reuse previous calculation
+////					res[j]=map.get(sarr[j]);
+////				} else {
+//					String boardLetters=sarr[j].replaceAll(".","");
+//					//create source letters string
+//					final String sourceLetters=rack+boardLetters;
+//					//check for matches if there enough sourceLetters
+//					if(boardLetters.length()+racklength>=length){
+//						//create freq array from the source
+//						final byte[] hasFreq=Help.createFreq(sourceLetters);
+//						//create hasChars from freq and wildcards
+//						final int[] hasChars=getHasChars(hasFreq);
+////							res.add("empty");
+//						//if there is no letters on board there is no pattern
+//						boolean skipPattern=boardLetters.length()==0;
+//						Pattern p=Pattern.compile(sarr[j]);
+//						
+//						//do heavy filtering, everything else 60 ms, everything else in this method 50 ms
+//						for(int i=0;i<wordlist.length;i++){
+//							if(wordlist[i].length()==length 
+//									&& hasNeededChars(i,hasChars,wildcards) && (skipPattern || p.matcher(wordlist[i]).matches())
+//									//(p.matcher(wordlist[i]).matches())
+//									//&& Help.correctCrossing(wordlist[i],j,fastCrossers,wf)
+//									&& Help.correctCrossing(wordlist[i], j, possible)
+//									){
+//								res[j].add(wordlist[i]);
+//							}
+//						}
+//					}
+//					//save the calculation so that it can be reused if the same pattern is used
+////					map.put(sarr[j], res[j]);
+////				}
+//			}
+//		}
+		return res;
+	} 
+	
+	public boolean matches(final String word, final String row, int index){
+		for(int i=0;i<word.length();i++,index++){
+			if(word.charAt(i)!='.'){
+				if(word.charAt(i)!=row.charAt(index)){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+//	public ArrayList<String>[] slowFilter3(String rack, String row,final int length,
+//			final String[][] fastCrossers,final boolean[] impossible,final WordFinder wf){
+//		int racklength=rack.length();
+//		rack=rack.toLowerCase();
+//		row=row.toLowerCase();
+//		int wildcards=rack.length();
+//		rack=rack.replaceAll("\\.", "");
+//		wildcards=wildcards-rack.length();
+//		
+//		HashMap<String, ArrayList<String>> map=new HashMap<String, ArrayList<String>>();
+//		
+//		final String [] sarr=MartinsFilterTest.getCharCombinations4(racklength,row,length,fastCrossers,impossible);
+////		System.out.println(Arrays.toString(sarr));
+////		System.out.println(length+Arrays.toString(sarr));
+//		@SuppressWarnings("unchecked")
+//		final ArrayList<String>[] res =new ArrayList[sarr.length];
+//		for(int j=0;j<res.length;j++){
+//			//initialize ArrayList
+//			res[j]=new ArrayList<String>();
+//			if(sarr[j]!=null){
+//				if(map.containsKey(sarr[j])){
+//					//reuse previous calculation
+//					res[j]=map.get(sarr[j]);
+//				} else {
+//					String boardLetters=sarr[j].replaceAll(".","");
+//					if(boardLetters.length()+racklength>=length){
+//						Pattern p=Pattern.compile(sarr[j]);
+//
+//						for(int i=0;i<wordlist.length;i++){
+//							if(wordlist[i].length()==length 
+//									&& p.matcher(wordlist[i]).matches()
+//									){
+//								res[j].add(wordlist[i]);
+//							}
+//						}
+//					}
+//					//save the calculation so that it can be reused if the same pattern is used
+//					map.put(sarr[j], res[j]);
+//				}
+//			}
+//		}
+//		return res;
+//	} 
+	
+
+	
+	
 	
 	public static String[] getCharCombinations(final String row,final int length){
 		final String[] sarr=new String[row.length()-length+1];
@@ -448,6 +762,64 @@ public class MartinsFilterTest {
 		return sarr;
 	}
 	
+	/**
+	 * row is the whole row.
+	 * length is the length of the words tested for.
+	 * crossing is a string array with all words that cross the row with a space for
+	 * the crossing point.
+	 * impossible is points that it's impossible to create a word by inserting one letter.
+	 * @param row
+	 * @param length
+	 * @param haveAdjacent
+	 * @param fastCrossers
+	 * @return
+	 */
+	public static String[] getCharCombinations4(final int rackLength, final String row,final int length,String[][] fastCrossers,boolean[] impossible){
+		final String[] sarr=new String[row.length()-length+1];
+		for(int i=0;i+length-1<row.length();i++){
+			//it must not be impossible
+			if(!impossibleInRange(i,length,impossible)){
+				String tmp=row.substring(i,i+length);
+				String tmp2=tmp.replaceAll(" ", "");
+				//must have room to place pieces
+				if(tmp.length()!=tmp2.length()){
+					//must have enough letters
+					if(length-tmp2.length()<=rackLength){
+						//it must be empty before and after the word, or else it's not the given length
+						if( (i==0 || row.charAt(i-1)==' ') && ((i+length)==row.length() || row.charAt(i+length)==' ')){
+							if(tmp2.length()==0){
+								//maybe should check if it's possible to complete each crossing word, and if not call the place forbidden
+								//check if there are letters above or below
+								for(int j=i;j<i+length;j++){
+									//must be letters above or below, if it's empty
+									if(fastCrossers[j]!=null){
+										sarr[i]=tmp.replaceAll(" ", ".");
+										break;
+									}
+								}
+							} else {
+								sarr[i]=tmp.replaceAll(" ", ".");
+							}
+
+						}
+					}
+
+				}
+			}
+		}
+		return sarr;
+	}
+	
+	private static boolean impossibleInRange(final int start,final int length,final boolean[] impossible){
+		final int limit=start+length;
+		for(int i=start;i<limit;i++){
+			if(impossible[i]){
+				return true;
+			}
+		}
+		return false;
+	}
+	
 //	public ArrayList<String> filter(String rack, String[] wordsOnRow){
 //		//to lowe case
 //		rack=rack.toLowerCase();
@@ -491,35 +863,31 @@ public class MartinsFilterTest {
 //			if(((neededChars[j][i]&hasChars[j])!=neededChars[j][i])){
 //				return false;
 //			}
-			if(!hasNeededChars(neededChars[j][i],hasChars[j])){
+			if(!Help.hasNeededChars(neededChars[j][i],hasChars[j])){
 				return false;
 			}
 		}
 		return true;
 	}
 	
-	/**
-	 * Checks that all the "neededChars" is in "hasChars". 
-	 * @param neededChars
-	 * @param hasChars
-	 * @return
-	 */
-	private static boolean hasNeededChars(final int neededChars,final int hasChars){
-		return (neededChars & hasChars) == neededChars;
-	}
-	
-	/**
-	 * help method for concatinating all strings in a string array.
-	 * Should be moved to a help methods class.
-	 * @param sarr
-	 * @return
-	 */
-	private String concatinate(final String[] sarr){
-		final StringBuilder sb=new StringBuilder();
-		for(int i=0;i<sarr.length;i++){
-			sb.append(sarr[i]);
+	private boolean hasNeededChars(final int i,final int hasChars[],final int wildcards){
+		final int limit=lengths[i]; 
+		if(limit>hasChars.length+wildcards){ return false; }
+		for(int j=0,j2=j+wildcards;j2<limit;j++,j2++){
+			if(!Help.hasNeededChars(neededChars[j2][i],hasChars[j])){
+				return false;
+			}
 		}
-		return sb.toString();
+		return true;
+//		final int limit=lengths[i]; 
+//		if(limit>hasChars.length+wildcards){ return false; }
+//		for(int j=0,j2=j+wildcards;j2<limit;j++,j2++){
+//			if(!Help.hasNeededChars(neededChars[j2][i],hasChars[j])){
+//				return false;
+//			}
+//		}
+//		return true;
 	}
+
 	
 }
